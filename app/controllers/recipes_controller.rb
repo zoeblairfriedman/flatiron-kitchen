097319f15d7b1c3605460cf338202d04d7c830f7
Zoe Friedman <zoeblair.friedman@gmail.com>
2021-03-20 15:51:21 -0400

    Done.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,11 +2,13 @@ class RecipesController < ApplicationController
 
     def new 
         @recipe = Recipe.new
+        @recipe.ingredients.build
+        @ingredients = Ingredient.all
     end
 
     def create
-        @recipe = Recipe.new(recipe_params)
         binding.pry
+        @recipe = Recipe.new(recipe_params)
         if @recipe.save
             redirect_to recipe_path(@recipe)
         else
@@ -14,12 +16,15 @@ class RecipesController < ApplicationController
         end
     end
 
+    def show
+        @recipe = Recipe.find_by(id: params[:id])
+    end
 
 
 private
 
     def recipe_params
-        params.require(:recipe).permit(:name)
+        params.require(:recipe).permit(:name, :ingredients_attributes => [:name])
     end
 
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,16 @@
 class Recipe < ApplicationRecord
     has_many :recipe_ingredients
     has_many :ingredients, through: :recipe_ingredients
+
+def ingredients_attributes=(ingredients_attributes)
+    binding.pry
+    ingredients_attributes.each do |ingredient_attribute|
+        self.ingredients.build(ingredient_attribute)
+    end
+end 
+
 end
+
+
+#when you get back to this, the issue is that it's coming in as a hash, not as an array. Avi's hack is not working for me
+
